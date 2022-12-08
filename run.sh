bdee9274fcb948d4492be9a9e618d3c43ffcae10
Guo Y.K <hi@guoyk.net>
2022-12-08 12:59:26 +0800

    update

---
+++ run.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+set -eu
+
+cd $(dirname $0)
+
+rm -f videos.list.txt
+
+ls -1 | grep MP4 | xargs -I {} bash -c "echo \"file '{}'\" >> videos.list.txt"
+
+rm -f combined.mp4
+
+ffmpeg -f concat -safe 0 -i videos.list.txt -c copy combined.mp4
+
+rm -f final.mp4
+
+ffmpeg -i combined.mp4 -i overlay.png -filter_complex '[0:v][1:v] overlay=0:0' -c:a copy final.mp4
+
+rm -f combined.mp4
