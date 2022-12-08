0e4c5446766116a8678e431116a55b147a458a49
Guo Y.K <hi@guoyk.net>
2022-12-08 18:17:45 +0800

    remove audio

--- run.sh
+++ run.sh
@@ -14,6 +14,6 @@ ffmpeg -f concat -safe 0 -i videos.list.txt -c copy combined.mp4
 
 rm -f final.mp4
 
-ffmpeg -i combined.mp4 -i overlay.png -filter_complex '[0:v][1:v] overlay=0:0' -c:v h264_videotoolbox -b:v 15M -c:a copy final.mp4
+ffmpeg -i combined.mp4 -i overlay.png -filter_complex '[0:v][1:v] overlay=0:0' -c:v h264_videotoolbox -b:v 15M -an final.mp4
 
 rm -f combined.mp4
